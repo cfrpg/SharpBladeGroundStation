d84a27659f1aa4b367ac96a6e9c7ad006165fdd0
cfrpg <726525185@qq.com>
2017-08-12 00:01:43 +0800

    Add Spd&Alt indicator

---
+++ PFDControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightDisplay
+{
+    /// <summary>
+    /// PFDControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class PFDControl : UserControl
+    {
+
+        public static readonly DependencyProperty FlightStateProperty =
+            DependencyProperty.Register("FlightState", typeof(FlightState), typeof(PFDControl), new PropertyMetadata(FlightState.Zero, PFDControl.OnFlightStatePropertyChanged));
+        public FlightState FlightState
+        {
+            get { return (FlightState)GetValue(FlightStateProperty); }
+            set { SetValue(FlightStateProperty, value); }
+        }
+        public static void OnFlightStatePropertyChanged(DependencyObject sender,DependencyPropertyChangedEventArgs e)
+        {
+            PFDControl pc = sender as PFDControl;
+            pc.OnFlightStateChanged(e);
+        }
+        public void OnFlightStateChanged(DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+
+
+        public PFDControl()
+        {
+            InitializeComponent();
+           
+
+        }
+    }
+}
