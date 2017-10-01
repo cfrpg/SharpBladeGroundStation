61dbe81bd74412cf4ad6f3f3e8c2299a50ab7400
cfrpg <726525185@qq.com>
2017-10-01 16:51:34 +0800

    ......

--- SharpBladeGroundStation/MainWindow.xaml.cs
+++ SharpBladeGroundStation/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SharpBladeGroundStation.CommLink;
 using SharpBladeGroundStation.Map;
+using SharpBladeGroundStation.DataStructs;
 using GMap.NET;
 using System.Diagnostics;
 
@@ -27,8 +28,11 @@ namespace SharpBladeGroundStation
         SerialLink link;
         LinkPackage package;
         string msg = "";
+		string linkStateMsg;
 
         PortScanner portscanner;
+
+		List<Vector3Data> sensorData;
 
         public MainWindow()
         {
@@ -39,6 +43,15 @@ namespace SharpBladeGroundStation
             portscanner = new PortScanner(LinkProtocol.ANOLink, 115200, 20480, 1);
             portscanner.OnFindPort += Portscanner_OnFindPort;
 			portscanner.Start();
+			linkStateMsg = "Connecting";
+
+			sensorData = new List<Vector3Data>();
+			sensorDataList.ItemsSource = sensorData;
+
+			Binding b = new Binding();
+			b.Source = linkStateMsg;
+			b.Mode = BindingMode.OneWay;
+			linkStateText.SetBinding(TextBlock.TextProperty, b);
         }
 
 		private void initGmap()
@@ -60,6 +73,7 @@ namespace SharpBladeGroundStation
 			link = e.Link;
 			link.OnReceivePackage += Link_OnReceivePackage;
 			link.OpenPort();
+			linkStateMsg = link.Port.PortName + ":" + link.Protocol.ToString();
         }
 
 		private void Link_OnReceivePackage(SerialLink sender, EventArgs e)
@@ -67,18 +81,95 @@ namespace SharpBladeGroundStation
             while (link.ReceivedPackageQueue.Count != 0)
             {
                 package = link.ReceivedPackageQueue.Dequeue();
-                
+				switch(sender.Protocol)
+				{
+					case LinkProtocol.MAVLink:
+
+						break;
+					case LinkProtocol.ANOLink:
+
+						break;
+				}
             }
         }
-
-		private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-
-		}
+		
 
 		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
 		{
 			leftcol.MaxWidth = (e.NewSize.Height-30)/669*300;
 		}
+
+		private void setSensorData(string name,double x,double y,double z)
+		{
+			for(int i=0;i<sensorData.Count;i++)
+			{
+				if(sensorData[i].Name==name)
+				{
+					sensorData[i].X = x;
+					sensorData[i].Y = y;
+					sensorData[i].Z = z;
+					sensorDataList.Items.Refresh();
+					return;
+				}
+			}
+			sensorData.Add(new Vector3Data(name, x, y, z));
+			sensorDataList.Items.Refresh();
+
+		}
+
+		private void analyzeANOPackage(LinkPackage p)
+		{
+			ANOLinkPackage package = (ANOLinkPackage)p;
+			switch(package.Function)
+			{
+				case 0x00://VER
+
+					break;
+				case 0x01://STATUS
+
+					break;
+				case 0x02://SENSER
+
+					break;
+				case 0x03://RCDATA
+
+					break;
+				case 0x04://GPSDATA
+
+					break;
+				case 0x05://POWER
+
+					break;
+				case 0x06://MOTO
+
+					break;
+				case 0x07://SENSER2
+
+					break;
+				case 0x0A://FLY MODEL
+
+					break;
+				case 0x0B://
+
+					break;
+				case 0x20://FP_NUMBER
+
+					break;
+				case 0x21://FP
+
+					break;
+				case 0xEF://CHECK
+
+					break;
+				default:
+					if(package.Function>=0x10&&package.Function<=0x15)
+					{
+
+					}
+					break;
+			}
+		}
+
+	
 	}
 }
