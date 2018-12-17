9c221b599d74d7f94dfd7783c2cc37d97051a117
cfrpg <726525185@qq.com>
2018-12-17 13:33:46 +0800

    sync

---
+++ src/SharpBladeGroundStation/DataStructs/Vehicle/FirmwareImage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace SharpBladeGroundStation.DataStructs
+{ 
+	public class FirmwareImage
+	{
+		Dictionary<string, string> fields;
+		byte[] image;
+		int imageSize;
+
+
+		public Dictionary<string, string> Fields
+		{
+			get { return fields; }
+			set { fields = value; }
+		}
+
+		/// <summary>
+		/// 固件本体的缓存，不要使用Length！
+		/// </summary>
+		public byte[] Image
+		{
+			get { return image; }
+			set { image = value; }
+		}
+
+		/// <summary>
+		/// 固件的实际长度
+		/// </summary>
+		public int ImageSize
+		{
+			get { return imageSize; }
+			set { imageSize = value; }
+		}
+
+		public FirmwareImage(string path)
+		{
+			StreamReader file = new StreamReader(path);
+			fields = new Dictionary<string, string>();
+			while(!file.EndOfStream)
+			{
+				string line = file.ReadLine();
+				if (line == "{" || line == "}")
+				{
+					continue;
+				}
+				string[] strs = line.Split(":".ToCharArray());
+				strs[0] = strs[0].Trim().Trim("\"".ToCharArray());
+				strs[1] = strs[1].Trim().TrimEnd(",".ToCharArray()).Trim().Trim("\"".ToCharArray());				
+				fields.Add(strs[0], strs[1]);
+			}
+			string imagestr = fields["image"];
+			byte[] buff = Convert.FromBase64String(imagestr);
+			int maxsize = int.Parse(fields["image_maxsize"]);
+			using (DeflateStream ds = new DeflateStream(new MemoryStream(buff, 2, buff.Length - 2), CompressionMode.Decompress))
+			{
+				image = new byte[maxsize];
+				imageSize = ds.Read(image, 0, maxsize);
+			}
+			
+			file.Close();
+		}
+	}
+}
--- src/SharpBladeGroundStation/MainWindow/firmwareUpdate.cs
+++ src/SharpBladeGroundStation/MainWindow/firmwareUpdate.cs
@@ -53,12 +53,94 @@ namespace SharpBladeGroundStation
 
 		private void updateFirmwareButton_Click(object sender, RoutedEventArgs e)
 		{
-
+			Thread updateThread = new Thread(new ThreadStart(bootloaderUpdateWorker));
+			updateFirmwareButton.IsEnabled = false;
+			firmwareUpdateMsgText.Text = "";
+			updateThread.IsBackground = true;
+			updateThread.Start();
 		}
 
 		private void firmwareMenuItem_Click(object sender, RoutedEventArgs e)
 		{
 
+		}
+
+		private void bootloaderUpdateWorker()
+		{			
+			portscanner.Stop();
+			Thread.Sleep(1000);
+			string[] existedPort = SerialPort.GetPortNames();
+			addUpdateMsg(string.Format("Existed port {0}:", existedPort.Count()));
+			
+			string newPortName = "";
+			bool flag = false;
+			while (!flag)
+			{
+				Thread.Sleep(500);
+				string[] currPorts = SerialPort.GetPortNames();
+				addUpdateMsg(string.Format("Current port {0}:", currPorts.Count()));
+				
+				foreach (var p in currPorts)
+				{
+					flag = true;
+					newPortName = p;
+					foreach (var s in existedPort)
+					{
+						if (p == s)
+						{
+							flag = false;
+							newPortName = "";
+							break;
+						}
+					}
+					if (flag)
+						break;
+				}
+			}
+			addUpdateMsg(string.Format("Find new port:{0}", newPortName));
+			
+
+			SerialPort port = new SerialPort(newPortName, 115200);
+			port.Open();
+
+			uint rev, bid, flashSize;			
+			addUpdateMsg(string.Format("Request BL REV"));
+			rev = getBoardInfo(port, BootloaderCmd.INFO_BL_REV);			
+			addUpdateMsg(string.Format("BL REV:{0}", rev));
+			
+			addUpdateMsg(string.Format("Request BOARD ID"));
+			bid = getBoardInfo(port, BootloaderCmd.INFO_BOARD_ID);
+			
+			addUpdateMsg(string.Format("BOARD ID:{0}", bid));
+			
+			
+			addUpdateMsg(string.Format("Request flash size"));
+			flashSize = getBoardInfo(port, BootloaderCmd.INFO_FLASH_SIZE);
+			
+			addUpdateMsg(string.Format("Flash size:{0}", flashSize));	
+
+			port.Close();
+
+		}
+
+		private uint getBoardInfo(SerialPort port,BootloaderCmd cmd)
+		{
+			uint res;
+			byte[] buf = new byte[128];
+			buf[0] = (byte)BootloaderCmd.PROTO_GET_DEVICE;
+			buf[1] = (byte)cmd;
+			buf[2] = (byte)BootloaderCmd.PROTO_EOC;
+			port.Write(buf, 0, 3);
+			while (port.BytesToRead < 4) ;
+			port.Read(buf, 0, port.BytesToRead);
+			res = BitConverter.ToUInt32(buf, 0);
+			return res;
+		}
+
+		private void addUpdateMsg(string str)
+		{
+			Action a = () => { firmwareUpdateMsgText.Text += str + Environment.NewLine; };
+			Dispatcher.BeginInvoke(a);
 		}
 	}
 }
